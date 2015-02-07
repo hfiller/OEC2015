7efc443fb0c9c74c51185102a18c2e5d21344578
Ian Wood <iwood@uwo.ca>
2015-02-06 22:10:06 -0500

    added zones

---
+++ js/main.js
@@ -0,0 +1,21 @@
+function blueZone(speed) {
+    return -0.35*speed + 100;
+}
+
+function redZone(speed) {
+    if (speed >= 51) {
+        return 1.4*speed + 50;
+    } else if (speed >= 31) {
+        return 1.25*speed + 50;
+    } else {
+        return 1.1*speed+50;
+    }
+ }
+
+ function yellowZone(speed) {
+    if (speed >= 31) {
+        return 2*redZone(speed) - 0.5*speed;
+    } else {
+        return 2*redZone(speed) - 0.4*speed
+    }
+ }

d2b595babfc0b6d34839de3d3b111da2a1c125ae
Ian Wood <iwood@uwo.ca>
2015-02-07 00:30:43 -0500

    added collision checking

--- js/main.js
+++ js/main.js
@@ -1,7 +1,11 @@
+// returns the size of the safe zone at the rear of a train
+// speed in km/h, returns meters
 function blueZone(speed) {
     return -0.35*speed + 100;
 }
 
+// returns the size of the danger zone at the front of a train
+// speed in km/h, returns meters
 function redZone(speed) {
     if (speed >= 51) {
         return 1.4*speed + 50;
@@ -12,14 +16,42 @@ function redZone(speed) {
     }
  }
 
- function yellowZone(speed) {
+// returns the size of the safe zone at the front of a train
+// speed in km/h, returns meters
+function yellowZone(speed) {
     if (speed >= 31) {
         return 2*redZone(speed) - 0.5*speed;
     } else {
         return 2*redZone(speed) - 0.4*speed
     }
- }
+}
 
+// determines if two trains will collide
+// requires first train (first parameter) is behind the location of second train (second parameter)
+function checkCollision(behindTrain, aheadTrain) {
+    // the tip of the safe zone of the behind train
+    var behindTrainYellowZone = frontOfBlock(behindTrain.FrtLoc) + yellowZone(behindTrain.Speed);
 
-var data = {};
-var currentTime = 0;
+    // the tip of the safe zone of the ahead train
+    var aheadTrainBlueZone = backOfBlock(aheadTrain.RearLoc) - blueZone(aheadTrain.Speed);
+
+    if (behindTrainYellowZone > aheadTrainBlueZone) { // collision with yellow zone
+        var behindTrainRedZone = frontOfBlock(behindTrain.FrtLoc) + redZone(behindTrain.Speed);
+
+        if (behindTrainRedZone > aheadTrainBlueZone) { // collision with red zone
+            // store in file behindTrain.Train + " STOP"
+            console.log("STOP");
+        } else {
+            // store in file behindTrain.Train + " SLOW DOWN"
+            console.log("SLOW DOWN");
+        }
+    } else {
+        console.log("no collision");
+    }
+
+}
+
+// Given a train location, returns the front of that block in meters
+function frontOfBlock(blockNumber) {
+    return 75 * blockNumber;
+}

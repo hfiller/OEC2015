8118c171003c0f471ba6d56926f8fa1392ba5322
Hannes Filler <hannes.filler@live.com>
2015-02-06 22:07:58 -0500

    fullscreen added

---
+++ js/fullscreen.js
@@ -0,0 +1,14 @@
+function fullscreen() {
+  if (container.requestFullscreen) {
+    container.requestFullscreen();
+  } else if (container.msRequestFullscreen) {
+    container.msRequestFullscreen();
+  } else if (container.mozRequestFullScreen) {
+    container.mozRequestFullScreen();
+  } else if (container.webkitRequestFullscreen) {
+    container.webkitRequestFullscreen();
+  }
+}
+
+var container = document.getElementById('container');
+container.addEventListener('click', fullscreen, false);
